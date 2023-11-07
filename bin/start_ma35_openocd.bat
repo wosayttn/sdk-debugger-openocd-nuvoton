af3357ace5ba2c15db2294b399d800b4238fb7dd
Wayne Lin <wclin@nuvoton.com>
2023-11-07 15:48:01 +0800

    Special version for MA35 aarch32

---
+++ bin/start_ma35_openocd.bat
@@ -0,0 +1,2 @@
+openocd.exe -f ../share/openocd/scripts/interface/cmsis-dap.cfg -f ../share/openocd/scripts/target/numicroMA35.cfg
+pause
--- bin/start_ma35d1_openocd.bat
+++
@@ -1,1 +0,0 @@
-openocd_cmsis-dap.exe -f ../scripts/interface/cmsis-dap.cfg -f ../scripts/target/numicroMA35D1.cfg -d3
